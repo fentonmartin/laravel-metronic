53628614f77e7a39acf70b480a5eba874f14dfcb
Fenton Martin <fentonxm@gmail.com>
2018-09-28 21:49:39 +0700

    Add register form validation

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -103,13 +103,31 @@ License: You must have a valid license purchased only from themeforest(the above
                             <!--begin::Form-->
                             <form class="m-login__form m-form" action="">
                                 <div class="form-group m-form__group">
-                                    <input class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" type="text" placeholder="Name" name="username" autocomplete="off">
+                                    <input class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" type="text" placeholder="Name" name="name" autocomplete="off">
+
+                                @if ($errors->has('name'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                @endif
                                 </div>
                                 <div class="form-group m-form__group">
-                                    <input class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" type="text" placeholder="Email" name="password">
+                                    <input class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" type="text" placeholder="Email" name="email">
+
+                                @if ($errors->has('email'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
                                 </div>
                                 <div class="form-group m-form__group">
                                     <input class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" type="password" placeholder="Password" name="password">
+
+                                @if ($errors->has('password'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
                                 </div>
                                 <div class="form-group m-form__group">
                                     <input class="form-control m-input m-login__form-input--last" type="password" placeholder="Password confirm" name="password_confirmation">
